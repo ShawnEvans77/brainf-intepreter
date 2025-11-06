8270b43cafc423532128dd62e1285288ee3d2466
ShawnEvans77 <shawnevans328@gmail.com>
2025-11-06 14:27:31 -0500

    update, works for hello world now

--- brainf.py
+++ brainf.py
@@ -1,29 +1,39 @@
+def execute(code):
+        p = 0
+        i = 0
+
+        while i < len(code):
+
+            match code[i]:
+                case '>':
+                    p += 1
+                case '<':
+                    p -= 1
+                case '+':
+                    array[p] += 1 if array[p] <= 255 else 255
+                case '-':
+                    array[p] -= 1 if array[p] > 0 else 0
+                case '.':
+                    print(chr(array[p]), end="")
+                case ',':
+                    array[p] = int(input())
+                case '[':
+                    ending_brace = code.index(']')
+                    starting_brace = i
+            
+                    if array[p] == 0:
+                        i = ending_brace 
+                case ']':
+                    if array[p] != 0:
+                        i = starting_brace 
+
+            i = i + 1
+
 file_path = "program.txt"
 array = [0] * 30000
-pointer = 0
-
-def execute(code):
-        for character in code:
-
-            match character:
-                case '>':
-                    pointer += 1
-                case '<':
-                    pointer -= 1
-                case '+':
-                    array[pointer] += 1
-                case '-':
-                    array[pointer] -= 1
-                case '.':
-                    print(chr(array[pointer]))
-                case ',':
-                    array[pointer] = int(input("enter integer: "))
-                case '[':
-                    ending_brace = program.index(']')
-
-                    while array[pointer] > 0:
-                        execute(program[ending_brace+1:])
 
 with open(file_path, "r") as file:
     program = file.read()
     execute(program)
+
+# print(array[:10])
