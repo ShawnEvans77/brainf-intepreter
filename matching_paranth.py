abcdc3a3749ffea6ecd71df9d07843d9237e2bb9
ShawnEvans77 <shawnevans328@gmail.com>
2025-11-12 00:49:38 -0500

    algo for brace matching

---
+++ matching_paranth.py
@@ -0,0 +1,30 @@
+# Given a string, with [ at specified index, determine its matching bracket. 
+# Instrumental for the jumping and implementing nested loops properly. 
+
+code = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+
+# Output:
+
+# Bracket '[' at index 0's matching brace ']' can be found at index 40.
+
+stack = []
+
+# for i, c in enumerate(code):
+#     match c:
+#         case '[':
+#             stack.append(i)
+#         case ']':
+#              print(f"Bracket '[' at index {stack.pop()}'s matching brace ']' can be found at index {i}")
+
+# ending = 0
+
+# for i, c in enumerate(code):
+#     match c:
+#         case '[':
+#             stack.append(i)
+#         case ']':
+#             stack.pop()
+#             ending = i
+
+# print(ending)
+
